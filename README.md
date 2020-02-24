8d182a96d2b210280828937684ccb7d62f7d0725
corneil <corneil.duplessis@gmail.com>
2020-02-24 16:54:09 +0200

    added visualization

--- README.md
+++
@@ -1,9 +0,0 @@
-# KFSM Turnstile Sample for Spring MVC
-
-A simple application to demonstrate implementing [KFSM](https://github.com/open-jumpco/kfsm) with the classic Turnstile FSM.
-
-```bash
-./gradlew bootRun
-```
-
-Visit http://localhost:8080/
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
+buildscript {
+    repositories {
+        mavenLocal()
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+        mavenCentral()
+    }
+    dependencies {
+        classpath("io.jumpco.kfsm.open:kfsm-viz-plugin:1.0.3")
+    }
+}
 plugins {
     id("org.springframework.boot") version "2.2.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
 }
-
+apply(plugin = "io.jumpco.open.kfsm.viz-plugin")
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
@@ -28,7 +39,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.jumpco.open:kfsm-jvm:1.0.0")
+    implementation("io.jumpco.open:kfsm-jvm:1.0.1")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
@@ -43,5 +54,19 @@ tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "1.8"
+    }
+}
+
+val assemble by tasks.existing {
+    dependsOn("generateFsmViz")
+}
+
+configure<io.jumpco.open.kfsm.gradle.VizPluginExtension> {
+    fsm("TurnstileFSM") {
+        outputFolder = file("generated")
+        input = file("src/main/kotlin/com/example/kfsm/Turnstile.kt")
+        isGeneratePlantUml = true // Required default is false
+        isGenerateAsciidoc = true // Required default is false
+        output = "turnstile"
     }
 }
