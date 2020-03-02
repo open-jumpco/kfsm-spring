634df162e9d6ac461ee4a2976f1eed92a3d46b85
corneil <corneil.duplessis@gmail.com>
2020-03-02 13:44:30 +0200

    updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-buildscript {
-    repositories {
-        mavenLocal()
-        maven {
-            url = uri("https://plugins.gradle.org/m2/")
-        }
-        mavenCentral()
-    }
-    dependencies {
-        classpath("io.jumpco.kfsm.open:kfsm-viz-plugin:1.0.4-SNAPSHOT")
-    }
-}
+
 plugins {
     id("org.springframework.boot") version "2.2.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
+    id("io.jumpco.open.kfsm.viz-plugin") version "1.0.7"
 }
-apply(plugin = "io.jumpco.open.kfsm.viz-plugin")
+
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
@@ -39,7 +29,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.jumpco.open:kfsm-jvm:1.0.1")
+    implementation("io.jumpco.open:kfsm-jvm:1.0.2")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
